513c039f9833942204395a57b4d50308f913055b
na <na@na.com>
2023-03-12 22:38:12 +0000

    Updated readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+Instructions:
+
+It is highly recommend you use this in conjunction with: https://github.com/Raine-gay/coqui-ai-TTS (A fork of coqui-ai TTS) In a virtualenv.
+
+python -m venv *envname*
+source *envname*/bin/activate
+pip install pip install git+https://github.com/Raine-gay/coqui-ai-TTS
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use pyo3::{prelude::*, types::PyDict};
+
+#[derive(Debug)]
+pub struct Synthesizer {
+    locals: Py<PyDict>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Synthesizer {
+    pub fn new(model_name: &str) -> Self {
+        
     }
 }
