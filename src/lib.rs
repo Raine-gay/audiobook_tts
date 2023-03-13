ca04122e5bf9fee0375fec78d88ab99299c4cc34
na <na@na.com>
2023-03-13 20:44:30 +0000

    Added the string filter.

--- src/lib.rs
+++ src/lib.rs
@@ -35,17 +35,63 @@ tts = TTS(model_name=model_name, progress_bar=False, gpu=True)
     }
 
     /// Synthesize a string into a wav file.
-    pub fn generate(&mut self, text_chunk: &str, wav_path: &str) -> Result<(), PyErr> {
-        Python::with_gil(|py| {
-            let locals = self.locals.as_ref(py);
-            locals.set_item("text_chunk", &text_chunk).unwrap();
-            locals.set_item("wav_path", wav_path).unwrap();
+    ///
+    /// Use the filter input bool if you wish for this crate to attempt to remove junk from the input.
+    /// It is recommend to use this feature as junk in the input can cause the TTS generator to crash.
+    /// Please note: That this feature hasn't been tested and so could have a lil bit of unintended behavior.
+    pub fn generate(
+        &mut self,
+        string_input: &str,
+        wav_path: &str,
+        filter_input: bool,
+    ) -> Result<(), PyErr> {
+        let mut string_input = string_input.to_string();
+        if filter_input {
+            string_input = filter_string_input(string_input);
+        }
+        if !string_input.is_empty() {
+            Python::with_gil(|py| {
+                let locals = self.locals.as_ref(py);
+                locals.set_item("text_chunk", &string_input).unwrap();
+                locals.set_item("wav_path", wav_path).unwrap();
 
-            py.run(
-                r#"tts.tts_to_file(text=text_chunk, file_path=wav_path)"#,
-                None,
-                Some(locals),
-            )
-        })
+                py.run(
+                    r#"tts.tts_to_file(text=text_chunk, file_path=wav_path)"#,
+                    None,
+                    Some(locals),
+                )
+            })?
+        }
+
+        Ok(())
     }
+}
+
+fn filter_string_input(string_input: String) -> String {
+    let mut input_chars: Vec<char> = string_input.chars().collect();
+
+    let mut contains_alphanumeric = false; // This checks
+    for char in &input_chars {
+        if char.is_ascii_alphanumeric() {
+            contains_alphanumeric = true;
+            break;
+        }
+    }
+    if !contains_alphanumeric {
+        return String::new();
+    }
+
+    const WHITELISTED_CHARS: [char; 46] = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+        '$', '£', '!', '.', '?', ',', '\x27', '&', ';', ':',
+    ];
+    for i in 0..input_chars.len() {
+        if !WHITELISTED_CHARS.contains(&input_chars[i]) {
+            input_chars[i] = '\x00';
+        }
+    }
+
+    let string_input: String = input_chars.into_iter().collect();
+    string_input.replace('\x00', "")
 }
